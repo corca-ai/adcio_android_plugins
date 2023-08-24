791326fb75aa17870626324488da506479d4dfa7
최민재 <cmjcmj1230@gmail.com>
2023-08-24 14:13:06 +0900

    feat: add initial project in folder
    2023-08-24

---
+++ app/src/main/java/ai/corca/adcio_android_plugins/MainActivity.kt
@@ -0,0 +1,11 @@
+package ai.corca.adcio_android_plugins
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
