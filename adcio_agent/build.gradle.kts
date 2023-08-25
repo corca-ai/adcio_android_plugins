4dea4d797df6016f35ed5ea308d3b33bd48f1cbd
yoohyunmyeong <aahspringaa4@naver.com>
2023-08-25 12:01:09 +0900

    build: add using dependency

--- adcio_agent/build.gradle.kts
+++ adcio_agent/build.gradle.kts
@@ -35,6 +35,9 @@ android {
 
 dependencies {
 
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.core:core-ktx:1.9.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
