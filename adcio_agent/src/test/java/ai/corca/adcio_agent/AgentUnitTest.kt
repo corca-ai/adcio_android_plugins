1d2e1aa4b1fc961e0a253b84d2119693a1a2f0c2
yoohyunmyeong <aahspringaa4@naver.com>
2023-08-25 12:00:18 +0900

    test: impl agent webview test

---
+++ adcio_agent/src/test/java/ai/corca/adcio_agent/AgentUnitTest.kt
@@ -0,0 +1,28 @@
+package ai.corca.adcio_agent
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class AgentUnitTest {
+    @Test
+    fun productId_isFetched() {
+
+        val webViewManager = WebViewManager(
+            context = null,
+            clientId = "",
+            "",
+        )
+
+        webViewManager.apply {
+            callAdcioAgent()
+
+            setDelayedString("productId")
+            assertEquals(getDelayedString(), "productId")
+        }
+    }
+}
