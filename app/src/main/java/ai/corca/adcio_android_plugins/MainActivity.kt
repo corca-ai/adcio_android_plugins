6fcc2075786f4b7fe2340a09042bc814f326eb2d
yoohyunmyeong <aahspringaa4@naver.com>
2023-08-28 10:44:41 +0900

    feat: add showToast selected product's id

--- app/src/main/java/ai/corca/adcio_android_plugins/MainActivity.kt
+++ app/src/main/java/ai/corca/adcio_android_plugins/MainActivity.kt
@@ -1,11 +1,14 @@
 package ai.corca.adcio_android_plugins
 
+import ai.corca.adcio_agent.WebViewManager
+import ai.corca.adcio_agent.productId
 import ai.corca.adcio_android_plugins.agent.AgentActivity
 import ai.corca.adcio_android_plugins.analytics.AnalyticsActivity
 import ai.corca.adcio_android_plugins.databinding.ActivityMainBinding
 import ai.corca.adcio_android_plugins.placement.PlacementActivity
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -30,6 +33,22 @@ class MainActivity : AppCompatActivity() {
         binding.btnPlacement.setOnClickListener {
             val intent = Intent(this, PlacementActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+    private val webViewManager = WebViewManager(
+        context = this,
+        clientId = "30cb6fd0-17a5-4c56-b144-fef67de81bef",
+        "https://agent-dev.adcio.ai",
+        R.id.frame
+    )
+
+    override fun onRestart() {
+        super.onRestart()
+        webViewManager.setDelayedString(productId)
+        val getProductId = webViewManager.getDelayedString()
+        if (getProductId != "") {
+            Toast.makeText(this, webViewManager.getDelayedString(), Toast.LENGTH_SHORT).show()
         }
     }
 }
