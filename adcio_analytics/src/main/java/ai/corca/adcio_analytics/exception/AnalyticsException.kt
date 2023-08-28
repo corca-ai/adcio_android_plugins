02ebc57d078ef6da970178da0f3e5b28aa594c9d
최민재 <cmjcmj1230@gmail.com>
2023-08-28 14:25:30 +0900

    feat: add adcio analytics user using function

--- adcio_analytics/src/main/java/ai/corca/adcio_analytics/exception/AnalyticsException.kt
+++ adcio_analytics/src/main/java/ai/corca/adcio_analytics/exception/AnalyticsException.kt
@@ -1,18 +1,12 @@
 package ai.corca.adcio_analytics.exception
 
 private const val NOT_INITIALIZED_EXCEPTION_MESSAGE = "Not initialized. (Call the init function.)"
-private const val EMPTY_ENV_EXCEPTION_MESSAGE = "No environment variables have been entered."
 private const val INVALID_URL_FORMAT_MESSAGE = "Invalid URL format."
 internal const val UNKNOWN_EXCEPTION_MESSAGE = "Unknown error has occurred."
 
 class NotInitializedException : RuntimeException() {
     override val message: String
         get() = NOT_INITIALIZED_EXCEPTION_MESSAGE
-}
-
-class EmptyEnvFileException : RuntimeException() {
-    override val message: String
-        get() = EMPTY_ENV_EXCEPTION_MESSAGE
 }
 
 class InvalidUrlFormatException : RuntimeException() {
