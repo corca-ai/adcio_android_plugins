ff59470492c1e8564023c14f0f0a752be5849b5a
yoohyunmyeong <aahspringaa4@naver.com>
2023-08-28 10:45:25 +0900

    style: change min code style

--- adcio_agent/src/test/java/ai/corca/adcio_agent/AgentUnitTest.kt
+++ adcio_agent/src/test/java/ai/corca/adcio_agent/AgentUnitTest.kt
@@ -16,6 +16,7 @@ class AgentUnitTest {
             context = null,
             clientId = "",
             "",
+            0
         )
 
         webViewManager.apply {
--- app/src/main/java/ai/corca/adcio_android_plugins/agent/AgentActivity.kt
+++ app/src/main/java/ai/corca/adcio_android_plugins/agent/AgentActivity.kt
@@ -1,7 +1,6 @@
 package ai.corca.adcio_android_plugins.agent
 
 import ai.corca.adcio_agent.WebViewManager
-import ai.corca.adcio_agent.productId
 import ai.corca.adcio_android_plugins.R
 import ai.corca.adcio_android_plugins.databinding.ActivityAgentBinding
 import android.os.Bundle
@@ -16,18 +15,12 @@ class AgentActivity : AppCompatActivity() {
         context = this,
         clientId = "30cb6fd0-17a5-4c56-b144-fef67de81bef",
         "https://agent-dev.adcio.ai",
+        R.id.frame
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_agent)
-
         webViewManager.callAdcioAgent()
-    }
-
-    override fun onResume() {
-        super.onResume()
-        webViewManager.setDelayedString(productId)
-        binding.tvMain.text = webViewManager.getDelayedString()
     }
 }
