a80c7c5b7be997e7e05725a72dea518f317507f3
yoohyunmyeong <aahspringaa4@naver.com>
2023-08-29 10:26:09 +0900

    feat: add compose adcio agent webview bridge

---
+++ adcio_agent_compose/src/main/java/ai/corca/adcio_agent_compose/agent/ComposeAdcioAgent.kt
@@ -0,0 +1,58 @@
+package ai.corca.adcio_agent_compose.agent
+
+import ai.corca.adcio_agent_compose.provider.ComposeWebViewManager
+import android.app.Activity
+import android.content.Context
+import android.webkit.JavascriptInterface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.google.accompanist.web.AccompanistWebChromeClient
+import com.google.accompanist.web.AccompanistWebViewClient
+import com.google.accompanist.web.WebView
+import com.google.accompanist.web.rememberWebViewState
+
+@Composable
+fun callAdcioAgent(
+    modifier: Modifier,
+    context: Context,
+    clientId: String,
+    baseUrl: String = "https://agent-dev.adcio.ai",
+    activity: Activity,
+) {
+    val startPage = "start/"
+    val agentUrl = "$baseUrl/$clientId/$startPage?platform=android"
+
+    val webViewState =
+        rememberWebViewState(
+            url = agentUrl,
+            additionalHttpHeaders = emptyMap()
+        )
+    val webViewClient = AccompanistWebViewClient()
+    val webChromeClient = AccompanistWebChromeClient()
+
+    WebView(
+        state = webViewState,
+        client = webViewClient,
+        chromeClient = webChromeClient,
+        onCreated = { webView ->
+            with(webView) {
+                settings.run {
+                    javaScriptEnabled = true
+                    domStorageEnabled = true
+                    javaScriptCanOpenWindowsAutomatically = false
+                }
+                addJavascriptInterface(ProductRouterJavascriptInterface(activity), "ProductRouter")
+            }
+        }
+    )
+}
+
+class ProductRouterJavascriptInterface(private val activity: Activity) {
+    @JavascriptInterface
+    fun postMessage(message: String) {
+        webViewManager.getProductId(message)
+        activity.finish()
+    }
+}
+
+val webViewManager = ComposeWebViewManager()
