a23c7f1b1d7a3c8348c6141674faa411cbec0d9a
yoohyunmyeong <aahspringaa4@naver.com>
2023-09-01 15:48:01 +0900

    style: add example view

---
+++ app/src/main/java/ai/corca/adcio_android_plugins/agent/ui/SecondActivity.kt
@@ -0,0 +1,12 @@
+package ai.corca.adcio_android_plugins.agent.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ai.corca.adcio_android_plugins.R
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
