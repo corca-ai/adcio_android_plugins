71d20179eaa30ab2345acb8c86bd1e2c6526f712
yoohyunmyeong <aahspringaa4@naver.com>
2023-09-01 15:49:11 +0900

    feat: add Xmlactivity example code

--- app/src/main/java/ai/corca/adcio_android_plugins/agent/ui/XmlActivity.kt
+++ app/src/main/java/ai/corca/adcio_android_plugins/agent/ui/XmlActivity.kt
@@ -5,7 +5,9 @@ import ai.corca.adcio_agent.provider.AdcioAgentListener
 import ai.corca.adcio_agent.provider.adcioAgentListener
 import ai.corca.adcio_android_plugins.R
 import ai.corca.adcio_android_plugins.databinding.ActivityXmlBinding
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -16,9 +18,7 @@ class XmlActivity : AppCompatActivity() {
     private val agent = AdcioAgent(
         context = this,
         clientId = "30cb6fd0-17a5-4c56-b144-fef67de81bef",
-        baseUrl = "https://agent-dev.adcio.ai",
-        fragmentContainer = R.id.adcio_webview_frame,
-        showAppBar = false
+        fragmentContainer = R.id.adcio_agent_frame,
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,8 +29,24 @@ class XmlActivity : AppCompatActivity() {
 
         adcioAgentListener = object : AdcioAgentListener {
             override fun onClickProductId(productId: String) {
+                /**
+                 * An example of a Toast for productId.
+                 */
                 Toast.makeText(this@XmlActivity, productId, Toast.LENGTH_SHORT).show()
+
+                /**
+                 * An example of screen movement for productId.
+                 */
+                val intent = Intent(this@XmlActivity, SecondActivity::class.java)
+                intent.putExtra("productId", productId)
+                startActivity(intent)
             }
+        }
+
+
+        binding.btnBack.setOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+            agent.agentGoBack()
         }
     }
 }
