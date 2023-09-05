68f2033fc1ec93b898ee1f9b907ad8f75151983e
Yu Hyun Myeong <77616769+aahspringaa4@users.noreply.github.com>
2023-09-05 09:47:51 +0900

    Feature/files_to_upload_mavencentral (#10)
    * build: add module's publish information
    
    * feat: add file to use upload mavencentral

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,20 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+    repositories {
+        maven(url = "https://plugins.gradle.org/m2")
+    }
+
+    dependencies {
+        classpath("io.github.gradle-nexus:publish-plugin:1.1.0")
+    }
+}
+
 plugins {
     id("com.android.application") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.0" apply false
     id("com.android.library") version "8.1.0" apply false
+    id("io.github.gradle-nexus.publish-plugin") version "1.1.0" apply true
 }
+
+apply(from = "${rootDir}/scripts/publish-root.gradle")
