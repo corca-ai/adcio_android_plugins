9a0c9938eb0d67a0b68ba1b5039e5240bd65f84b
yoohyunmyeong <aahspringaa4@naver.com>
2023-09-05 09:36:20 +0900

    build: add module's publish information

--- adcio_agent/build.gradle.kts
+++ adcio_agent/build.gradle.kts
@@ -4,6 +4,14 @@ plugins {
     id("org.jlleitschuh.gradle.ktlint") version "10.2.0"
 }
 
+rootProject.extra.apply {
+    set("PUBLISH_GROUP_ID", "io.github.corca-ai")
+    set("PUBLISH_ARTIFACT_ID", "adcio_agent")
+    set("PUBLISH_VERSION", "0.0.1")
+    set("PUBLISH_SCM_URL", "https://github.com/corca-ai/adcio_android_plugins")
+    set("PUBLISH_DESCRIPTION", "ADCIO_Agent")
+}
+
 android {
     namespace = "ai.corca.adcio_agent"
     compileSdk = 33
@@ -32,6 +40,8 @@ android {
         jvmTarget = "1.8"
     }
 }
+
+apply(from = "${rootProject.projectDir}/scripts/publish-module.gradle")
 
 dependencies {
 
