c42c22db363476aeae32b92ac21d0b99b70b5ee2
최민재 <cmjcmj1230@gmail.com>
2023-09-05 12:09:31 +0900

    chore: change base url to release

--- adcio_placement/src/main/java/ai/corca/adcio_placement/network/PlacementUrl.kt
+++ adcio_placement/src/main/java/ai/corca/adcio_placement/network/PlacementUrl.kt
@@ -2,7 +2,7 @@ package ai.corca.adcio_placement.network
 
 internal object PlacementUrl {
 
-    var baseUrl: String = "https://api-dev.adcio.ai/"
+    var baseUrl: String = "https://api.adcio.ai/"
 
     object EndPoint {
         const val suggestions = "suggestions"
