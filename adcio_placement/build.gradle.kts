931c3a85a6fd38c0f94094897dffa5035ac06db0
yoohyunmyeong <aahspringaa4@naver.com>
2023-09-06 15:21:12 +0900

    build: update plugin version

--- adcio_placement/build.gradle.kts
+++ adcio_placement/build.gradle.kts
@@ -5,9 +5,9 @@ plugins {
 }
 
 rootProject.extra.apply {
-    set("PUBLISH_GROUP_ID", "io.github.corca-ai")
+    set("PUBLISH_GROUP_ID", "adcio.ai")
     set("PUBLISH_ARTIFACT_ID", "adcio_placement")
-    set("PUBLISH_VERSION", "0.0.1")
+    set("PUBLISH_VERSION", "0.1.1")
     set("PUBLISH_SCM_URL", "https://github.com/corca-ai/adcio_android_plugins")
     set("PUBLISH_DESCRIPTION", "ADCIO_Placement")
 }
