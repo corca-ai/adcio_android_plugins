f27fada360ebec6107db0d80a531cb896992893d
yoohyunmyeong <aahspringaa4@naver.com>
2023-09-14 14:25:40 +0900

    build: change plugin dependency method

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,8 +68,8 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.1")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.1")
 
-    implementation(project(path = ":adcio_agent"))
-    implementation(project(path = ":adcio_agent_compose"))
-    implementation(project(path = ":adcio_placement"))
-    implementation(project(path = ":adcio_analytics"))
+    implementation("io.github.corca-ai:adcio_analytics:0.1.0")
+    implementation("io.github.corca-ai:adcio_placement:0.1.0")
+    implementation("io.github.corca-ai:adcio_agent:0.1.0")
+    implementation("io.github.corca-ai:adcio_agent_compose:0.1.1")
 }
