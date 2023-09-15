d0fd42c426575874997b5f914edcec39c07f590f
yoohyunmyeong <aahspringaa4@naver.com>
2023-09-15 10:48:40 +0900

    build: add core module dependency in agent

--- adcio_agent_compose/build.gradle.kts
+++ adcio_agent_compose/build.gradle.kts
@@ -61,4 +61,6 @@ dependencies {
     implementation("androidx.compose.ui:ui:1.5.0")
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation("androidx.navigation:navigation-compose:2.7.1")
+
+    implementation(project(path = ":adcio_core"))
 }
