65c08d8655d38829906f278c2dd0dd48b05564b4
yoohyunmyeong <aahspringaa4@naver.com>
2023-09-19 16:55:01 +0900

    build: change product module dependency -> library dependency

--- adcio_agent/build.gradle.kts
+++ adcio_agent/build.gradle.kts
@@ -53,5 +53,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    implementation(project(path = ":adcio_core"))
+    implementation("io.github.corca-ai:adcio_core:0.1.0")
 }
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,9 +68,10 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.1")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.1")
 
+    implementation("io.github.corca-ai:adcio_core:0.1.0")
+
     implementation(project(path = ":adcio_agent"))
     implementation(project(path = ":adcio_agent_compose"))
     implementation(project(path = ":adcio_placement"))
     implementation(project(path = ":adcio_analytics"))
-    implementation(project(path = ":adcio_core"))
 }
