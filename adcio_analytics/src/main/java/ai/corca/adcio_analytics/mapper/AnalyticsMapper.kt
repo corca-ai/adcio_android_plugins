38263d8dccf79cac8577337c78b8944c301b2410
Yu Hyun Myeong <77616769+aahspringaa4@users.noreply.github.com>
2023-09-21 22:55:44 -0700

    Feature/refactor pageview argumentparam (#39)
    * refactor: change pageview parameter dataclass to individual  argument
    
    * refactor: delete don't use mapper
    
    * refactor: change id value menage inner method
    
    * style: delete unnecessary code
    
    * refactor: remove nullable from notnull function

--- adcio_analytics/src/main/java/ai/corca/adcio_analytics/mapper/AnalyticsMapper.kt
+++ adcio_analytics/src/main/java/ai/corca/adcio_analytics/mapper/AnalyticsMapper.kt
@@ -1,9 +1,7 @@
 package ai.corca.adcio_analytics.mapper
 
 import ai.corca.adcio_analytics.model.AdcioLogOption
-import ai.corca.adcio_analytics.model.AnalyticsPageViewOption
 import ai.corca.adcio_analytics.network.data.AnalyticsRequest
-import ai.corca.adcio_analytics.network.data.pageview.AnalyticsPageViewRequest
 
 internal fun AnalyticsRequest.toLogOption(): AdcioLogOption = AdcioLogOption(
     requestId = this.requestId,
@@ -17,15 +15,3 @@ internal fun AdcioLogOption.toAnalyticsRequest(
     adsetId = this.adsetId,
     amount = amount,
 )
-
-internal fun AnalyticsPageViewOption.toAnalyticsPageViewRequest(): AnalyticsPageViewRequest =
-    AnalyticsPageViewRequest(
-        sessionId = this.sessionId,
-        deviceId = this.deviceId,
-        customerId = this.customerId,
-        storeId = this.storeId,
-        productCode = this.productCode,
-        path = this.path,
-        title = this.title ?: this.path,
-        referrer = this.referrer,
-    )
