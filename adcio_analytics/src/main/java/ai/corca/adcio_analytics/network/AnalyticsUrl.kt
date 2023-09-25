3860f91fd4ff930438f1674e09b2084c99854dd8
Yu Hyun Myeong <77616769+aahspringaa4@users.noreply.github.com>
2023-09-25 01:47:42 -0700

    Feature/Analytics AddToCart & Update Scheme (#47)
    * refactor: change analytics purchase endpoint
    
    * style: delete dont use import
    
    * feat: update add to cart func
    
    * refactor: update request data class
    
    * build: add internet permission
    
    * feat: add analytics method test code
    
    * feat: add test case param sample value
    
    * refactor: delete not necessary test code
    
    * refactor: change storeId is non-nullable

--- adcio_analytics/src/main/java/ai/corca/adcio_analytics/network/AnalyticsUrl.kt
+++ adcio_analytics/src/main/java/ai/corca/adcio_analytics/network/AnalyticsUrl.kt
@@ -9,7 +9,8 @@ internal object AnalyticsUrl {
     object EndPoint {
         const val impression = "$PERFORMANCE/impression"
         const val click = "$PERFORMANCE/click"
-        const val purchase = "$PERFORMANCE/purchase"
+        const val purchase = "$EVENTS/purchase"
         const val pageView = "$EVENTS/view"
+        const val addToCart = "$EVENTS/add-to-cart"
     }
 }
--- adcio_analytics/src/main/java/ai/corca/adcio_analytics/network/service/AnalyticsService.kt
+++ adcio_analytics/src/main/java/ai/corca/adcio_analytics/network/service/AnalyticsService.kt
@@ -1,8 +1,10 @@
 package ai.corca.adcio_analytics.network.service
 
 import ai.corca.adcio_analytics.network.AnalyticsUrl
-import ai.corca.adcio_analytics.network.data.AnalyticsRequest
-import ai.corca.adcio_analytics.network.data.pageview.AnalyticsPageViewRequest
+import ai.corca.adcio_analytics.network.data.AnalyticsPerformanceRequest
+import ai.corca.adcio_analytics.network.data.event.AnalyticsAddToCartRequest
+import ai.corca.adcio_analytics.network.data.event.AnalyticsPageViewRequest
+import ai.corca.adcio_analytics.network.data.event.AnalyticsPurchaseRequest
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.POST
@@ -11,21 +13,26 @@ interface AnalyticsService {
 
     @POST(AnalyticsUrl.EndPoint.impression)
     fun onImpression(
-        @Body analyticsRequest: AnalyticsRequest
+        @Body analyticsPerformanceRequest: AnalyticsPerformanceRequest
     ): Call<Unit>
 
     @POST(AnalyticsUrl.EndPoint.click)
     fun onClick(
-        @Body analyticsRequest: AnalyticsRequest
+        @Body analyticsPerformanceRequest: AnalyticsPerformanceRequest
     ): Call<Unit>
 
     @POST(AnalyticsUrl.EndPoint.purchase)
     fun onPurchase(
-        @Body analyticsRequest: AnalyticsRequest
+        @Body analyticsPurchaseRequest: AnalyticsPurchaseRequest
     ): Call<Unit>
 
     @POST(AnalyticsUrl.EndPoint.pageView)
     fun onPageView(
         @Body analyticsPageViewRequest: AnalyticsPageViewRequest
+    ): Call<Unit>
+
+    @POST(AnalyticsUrl.EndPoint.addToCart)
+    fun onAddToCart(
+        @Body analyticsAddToCartRequest: AnalyticsAddToCartRequest
     ): Call<Unit>
 }
