410c73d7d44f3774ca3c29a1f6e42d8c5dfaf3a5
Yu Hyun Myeong <77616769+aahspringaa4@users.noreply.github.com>
2023-11-22 17:27:34 +0900

    feat: update analytics performance schema (#61)

--- adcio_analytics/src/main/java/ai/corca/adcio_analytics/network/data/AnalyticsPerformanceRequest.kt
+++ adcio_analytics/src/main/java/ai/corca/adcio_analytics/network/data/AnalyticsPerformanceRequest.kt
@@ -3,6 +3,10 @@ package ai.corca.adcio_analytics.network.data
 import com.google.gson.annotations.SerializedName
 
 data class AnalyticsPerformanceRequest(
+    @SerializedName("sessionId") val sessionId: String,
+    @SerializedName("deviceId") val deviceId: String,
+    @SerializedName("customerId") val customerId: String?,
+    @SerializedName("storeId") val storeId: String,
     @SerializedName("requestId") val requestId: String,
     @SerializedName("adsetId") val adsetId: String,
 ) {
