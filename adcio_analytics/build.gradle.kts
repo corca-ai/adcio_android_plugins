0cee8e21370c1a932ad3ec9c5d022574dc2a3015
yoohyunmyeong <aahspringaa4@naver.com>
2023-11-23 17:45:18 +0900

    build: update analytics version up to 0.1.3

--- adcio_analytics/build.gradle.kts
+++ adcio_analytics/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 rootProject.extra.apply {
     set("PUBLISH_GROUP_ID", "io.github.corca-ai")
     set("PUBLISH_ARTIFACT_ID", "adcio_analytics")
-    set("PUBLISH_VERSION", "0.1.2")
+    set("PUBLISH_VERSION", "0.1.3")
     set("PUBLISH_SCM_URL", "https://github.com/corca-ai/adcio_android_plugins")
     set("PUBLISH_DESCRIPTION", "adcio_analytics is log event collection")
 }
