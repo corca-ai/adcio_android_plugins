673c91bd4a1fb5a0b4d356dec7408a3b4edaa483
yoohyunmyeong <aahspringaa4@naver.com>
2023-12-14 16:22:23 +0900

    refactor: change exceptions to personal exception

--- adcio_placement/src/main/java/ai/corca/adcio_placement/exception/Error.kt
+++ adcio_placement/src/main/java/ai/corca/adcio_placement/exception/Error.kt
@@ -1,27 +1,21 @@
 package ai.corca.adcio_placement.exception
 
-internal const val UNKNOWN_EXCEPTION_MESSAGE = "Unknown error occurred"
-
-open class PlatformException(
-    open val code: String,
-    open val errorMessage: String
-) : RuntimeException() {
-    override fun toString(): String {
-        return "PlatformException(code='$code', errorMessage='$errorMessage')"
-    }
-}
-
 data class UnregisteredIdException(
-    override val code: String = "PLACEMENT_NOT_FOUND",
-    override val errorMessage: String = "placementID is not registered"
-) : PlatformException(code, errorMessage)
+    val code: String = "PLACEMENT_NOT_FOUND",
+    val errorMessage: String = "placementID is not registered",
+) : RuntimeException()
 
 data class DisabledPlacementException(
-    override val code: String = "PLACEMENT_NOT_ACTIVATED",
-    override val errorMessage: String = "The placement is not active"
-) : PlatformException(code, errorMessage)
+    val code: String = "PLACEMENT_NOT_ACTIVATED",
+    val errorMessage: String = "The placement is not active",
+) : RuntimeException()
 
-data class BadRequestException(
-    override val code: String = "BAD_REQUEST",
-    override val errorMessage: String
-) : PlatformException(code, errorMessage)
+data class BadRequestException (
+    val code: String = "BAD_REQUEST",
+    val errorMessage: String = "placementId must be a UUID",
+) : RuntimeException()
+
+data class UnKnownException(
+    val code: String = "UNKNOWN_ERROR",
+    val errorMessage: String,
+) : RuntimeException()
