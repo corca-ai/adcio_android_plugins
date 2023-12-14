c520a8f764c7a5e3ab01cdc0717bd66519116b00
yoohyunmyeong <aahspringaa4@naver.com>
2023-12-14 16:22:49 +0900

    refactor: remove toException() function

--- adcio_placement/src/main/java/ai/corca/adcio_placement/network/data/ErrorResponse.kt
+++ adcio_placement/src/main/java/ai/corca/adcio_placement/network/data/ErrorResponse.kt
@@ -1,15 +1,8 @@
 package ai.corca.adcio_placement.network.data
 
-import ai.corca.adcio_placement.exception.PlatformException
 import com.google.gson.annotations.SerializedName
 
 data class ErrorResponse(
     @SerializedName("statusCode") val statusCode: Int,
     @SerializedName("message") val message: Int,
 )
-
-fun ErrorResponse.toException(): PlatformException =
-    PlatformException(
-        code = statusCode.toString(),
-        errorMessage = message.toString(),
-    )
