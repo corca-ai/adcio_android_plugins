b85471d846e62b606b0ce9737ad3e8b8fd4fb7d9
yoohyunmyeong <aahspringaa4@naver.com>
2024-01-19 11:12:56 +0900

    feat: add application constants

---
+++ build-logic/src/main/kotlin/ai/corca/convention/ApplicationConstants.kt
@@ -0,0 +1,11 @@
+package ai.corca.convention
+
+import org.gradle.api.JavaVersion
+internal object ApplicationConstants {
+    const val minSdk = 24
+    const val targetSdk = 33
+    const val compileSdk = 33
+    const val versionCode = 32
+    const val versionName = "1.4.0"
+    val javaVersion = JavaVersion.VERSION_17
+}
