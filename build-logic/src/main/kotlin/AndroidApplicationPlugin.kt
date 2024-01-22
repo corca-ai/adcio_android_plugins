8352200e0726eeca4205cf8328664a875bb50eaf
yoohyunmyeong <aahspringaa4@naver.com>
2024-01-22 17:26:55 +0900

    feat: add application version setting convention plugin

---
+++ build-logic/src/main/kotlin/AndroidApplicationPlugin.kt
@@ -0,0 +1,35 @@
+@file:Suppress("UnstableApiUsage")
+
+import ai.corca.convention.ApplicationConstants
+import com.android.build.gradle.internal.dsl.BaseAppModuleExtension
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+
+internal class AndroidApplicationPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            applyPlugins(
+                PluginEnum.AndroidApplication,
+                PluginEnum.KotlinAndroid,
+            )
+
+            extensions.configure<BaseAppModuleExtension> {
+                configureApplication(this)
+
+                defaultConfig {
+                    targetSdk = ApplicationConstants.targetSdk
+                    versionName = ApplicationConstants.versionName
+                    versionCode = ApplicationConstants.versionCode
+                }
+
+                 buildTypes {
+                     release {
+                         isShrinkResources = true
+                         isMinifyEnabled = true
+                     }
+                 }
+            }
+        }
+    }
+}
