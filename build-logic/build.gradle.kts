d89fef9b2acfc159193b5321316b1c6226dd04cc
yoohyunmyeong <aahspringaa4@naver.com>
2024-01-22 17:24:18 +0900

    build: add build logic gradle's implement

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -1,11 +1,24 @@
 @file:Suppress("DSL_SCOPE_VIOLATION")
 
+import DependencyHandler.Extensions.implementations
+
 plugins {
     `kotlin-dsl`
+    alias(libs.plugins.kotlin.dokka)
+    alias(libs.plugins.util.dependency.handler.extensions)
+    alias(libs.plugins.local.convention.enum)
 }
 
 group = "ai.corca.local"
 
 dependencies {
-
+    implementations(
+        libs.kotlin.core,
+        libs.kotlin.dokka.base,
+        libs.kotlin.dokka.plugin,
+        libs.build.gradle.agp,
+        libs.build.dependency.handler.extensions,
+        libs.build.local.plugin.enum
+    )
+    compileOnly(libs.ksp.gradlePlugin)
 }
