88daaa0a04a230b67b7d227b8f8b06762ee3be9c
yoohyunmyeong <aahspringaa4@naver.com>
2024-01-25 16:30:37 +0900

    build: update application version

--- build-logic/src/main/kotlin/ai/corca/convention/ApplicationConstants.kt
+++ build-logic/src/main/kotlin/ai/corca/convention/ApplicationConstants.kt
@@ -3,9 +3,9 @@ package ai.corca.convention
 import org.gradle.api.JavaVersion
 
 internal object ApplicationConstants {
-    const val minSdk = 24
-    const val targetSdk = 33
-    const val compileSdk = 33
+    const val minSdk = 23
+    const val targetSdk = 34
+    const val compileSdk = 34
     const val versionCode = 32
     const val versionName = "1.0"
     val javaVersion = JavaVersion.VERSION_17
