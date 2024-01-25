63cf8af69248834a627bc704fba95c93bbefdf5e
yoohyunmyeong <aahspringaa4@naver.com>
2024-01-25 16:20:34 +0900

    build: update agent gradle by convention plugin

--- adcio_agent/build.gradle.kts
+++ adcio_agent/build.gradle.kts
@@ -1,7 +1,10 @@
+@file:Suppress("DSL_SCOPE_VIOLATION", "UnstableApiUsage")
+
 plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
-    id("org.jlleitschuh.gradle.ktlint") version "10.2.0"
+    id(ConventionEnum.AndroidLibrary)
+    id(ConventionEnum.JvmJUnit4)
+    id(libs.plugins.code.ktlint.get().pluginId)
+    id("maven-publish")
 }
 
 rootProject.extra.apply {
@@ -14,44 +17,19 @@ rootProject.extra.apply {
 
 android {
     namespace = "ai.corca.adcio_agent"
-    compileSdk = 33
-
-    defaultConfig {
-        minSdk = 24
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
 }
 
-apply(from = "${rootProject.projectDir}/scripts/publish-module.gradle")
+apply(from = "$rootDir/scripts/publish-module.gradle")
 
 dependencies {
 
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.core:core-ktx:1.10.1")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.material)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.core.ktx)
 
-    implementation("io.github.corca-ai:adcio_core:0.1.3")
+    androidTestImplementation(libs.test.ext.junit)
+    androidTestImplementation(libs.test.espresso)
+
+    implementation(libs.adcio.core)
 }
