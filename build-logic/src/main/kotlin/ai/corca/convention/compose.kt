d6f7a6934adf0e46d3a09c383a0cfcc51a19e2ec
yoohyunmyeong <aahspringaa4@naver.com>
2024-01-25 16:28:15 +0900

    feat: add compose plugin convention

--- build-logic/src/main/kotlin/ai/corca/convention/compose.kt
+++ build-logic/src/main/kotlin/ai/corca/convention/compose.kt
@@ -1,4 +1,24 @@
 @file:Suppress("UnstableApiUsage")
 
-// internal fun Project.configureComposeO(extension: CommenExtension)8
+import DependencyHandler.Extensions.implementations
+import com.android.build.api.dsl.CommonExtension
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
 
+internal fun Project.configureCompose(extension: CommonExtension<*, *, *, *>) {
+    extension.apply {
+        buildFeatures {
+            compose = true
+        }
+
+        composeOptions {
+            kotlinCompilerExtensionVersion = libs.findVersion("compose-compiler").get().toString()
+        }
+
+        dependencies {
+            implementations(
+                libs.findBundle("compose-core").get()
+            )
+        }
+    }
+}
