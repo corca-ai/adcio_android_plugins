566cdfef8a426d2a3b56cddbfa417638a2b6e2c6
yoohyunmyeong <aahspringaa4@naver.com>
2024-01-25 16:24:42 +0900

    build: update common project gradle setting
    set ktlint & detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,76 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
+@file:Suppress("DSL_SCOPE_VIOLATION", "PropertyName")
+
+plugins {
+    alias(libs.plugins.code.ktlint)
+    alias(libs.plugins.code.detekt)
+    alias(libs.plugins.kotlin.dokka)
+    alias(libs.plugins.local.convention.enum)
+    alias(libs.plugins.local.plugin.enum)
+    alias(libs.plugins.util.dependency.handler.extensions)
+    alias(libs.plugins.publish.plugin)
+}
+
 buildscript {
     repositories {
-        maven(url = "https://plugins.gradle.org/m2")
+        google()
+        mavenLocal()
+        mavenCentral()
+        gradlePluginPortal()
     }
 
     dependencies {
-        classpath("io.github.gradle-nexus:publish-plugin:1.1.0")
+        classpath(libs.kotlin.core)
+        classpath(libs.kotlin.dokka.base)
+        classpath(libs.build.gradle.agp)
     }
 }
 
-plugins {
-    id("com.android.application") version "8.1.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.0" apply false
-    id("com.android.library") version "8.1.0" apply false
-    id("io.github.gradle-nexus.publish-plugin") version "1.1.0" apply true
+allprojects {
+    apply {
+        plugin(rootProject.libs.plugins.code.ktlint.get().pluginId)
+        plugin(rootProject.libs.plugins.local.convention.enum.get().pluginId)
+        plugin(rootProject.libs.plugins.util.dependency.handler.extensions.get().pluginId)
+    }
+
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
 
 apply(from = "${rootDir}/scripts/publish-root.gradle")
+
+subprojects {
+    @Suppress("UnstableApiUsage")
+    if (
+        gradle.startParameter.isConfigureOnDemand &&
+        buildscript.sourceFile?.extension?.lowercase() == "kts" &&
+        parent != rootProject
+    ) {
+        generateSequence(parent) { project ->
+            project.parent.takeIf { parent ->
+                parent != rootProject
+            }
+        }.forEach { project ->
+            evaluationDependsOn(project.path)
+        }
+    }
+
+    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+        version.set(rootProject.libs.versions.plugin.code.ktlint.core.get())
+        android.set(true)
+        outputToConsole.set(true)
+
+        @Suppress("DEPRECATION")
+        additionalEditorconfigFile.set(file("$rootDir/.editorconfig"))
+    }
+}
+
+tasks.register(
+    name = "cleanAll",
+    type = Delete::class
+) {
+    allprojects.map(Project::getBuildDir).forEach(::delete)
+}
