fdd81344281c3065dca36ba86b5a3dc6c891177f
yoohyunmyeong <aahspringaa4@naver.com>
2024-02-14 16:10:47 +0900

    build: remove adcio core in agent gradle

--- adcio_agent/build.gradle.kts
+++ adcio_agent/build.gradle.kts
@@ -52,6 +52,4 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
-    implementation("io.github.corca-ai:adcio_core:0.1.3")
 }
--- adcio_agent_compose/build.gradle.kts
+++ adcio_agent_compose/build.gradle.kts
@@ -60,7 +60,4 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview:1.5.0")
     implementation("androidx.compose.ui:ui:1.5.0")
     implementation("androidx.activity:activity-compose:1.7.2")
-    implementation("androidx.navigation:navigation-compose:2.7.1")
-
-    implementation("io.github.corca-ai:adcio_core:0.1.3")
 }
