1c1612e68c63e3bebd537ba611f387ab6d214929
yoohyunmyeong <aahspringaa4@naver.com>
2024-06-19 16:15:22 +0900

    feat

--- dacs/build.gradle.kts
+++ dacs/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
+
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
