db9f98f970df6a3869c57e64afd835c7c3eca450
yoohyunmyeong <aahspringaa4@naver.com>
2024-06-19 15:19:32 +0900

    feat

--- dacs/build.gradle.kts
+++ dacs/build.gradle.kts
@@ -24,16 +24,18 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 }
 
 dependencies {
 
+    implementation(kotlin("stdlib"))
+    
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
