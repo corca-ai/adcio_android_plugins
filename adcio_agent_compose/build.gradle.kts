89d80fbc91e5d5ae8dc01207c3ae7a41f6cb2130
yoohyunmyeong <aahspringaa4@naver.com>
2024-06-19 15:55:49 +0900

    feat

--- adcio_agent_compose/build.gradle.kts
+++ adcio_agent_compose/build.gradle.kts
@@ -38,11 +38,11 @@ android {
         kotlinCompilerExtensionVersion = "1.4.0"
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
 }
 
