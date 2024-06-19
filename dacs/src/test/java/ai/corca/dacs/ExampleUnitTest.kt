070d42d1dd2297bc4aef403383651dde902cdf0f
yoohyunmyeong <aahspringaa4@naver.com>
2024-06-19 16:07:16 +0900

    ㄹ

--- dacs/src/test/java/ai/corca/dacs/ExampleUnitTest.kt
+++ dacs/src/test/java/ai/corca/dacs/ExampleUnitTest.kt
@@ -1,19 +1,2 @@
 package ai.corca.dacs
 
-import kotlinx.coroutines.test.runTest
-import org.junit.Test
-
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-
-class ExampleUnitTest {
-
-    @Test
-    fun testGreet() {
-        val dacsProvider = DacsProvider()
-        dacsProvider.greet()
-    }
-}
