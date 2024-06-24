13c2a7cb78c4ee3d0643ba165118c82e1ffcd0e9
yoohyunmyeong <aahspringaa4@naver.com>
2024-06-24 16:03:07 +0900

    feat

--- dacs/src/test/java/ai/corca/dacs/DacsProviderTest.kt
+++ dacs/src/test/java/ai/corca/dacs/DacsProviderTest.kt
@@ -1,2 +1,13 @@
 package ai.corca.dacs
 
+import org.junit.Test
+
+class DacsProviderTest {
+
+    @Test
+    fun testGreet() {
+        val provider = DacsProvider()
+        val result = provider.greet()
+        println("Result: $result")
+    }
+}
