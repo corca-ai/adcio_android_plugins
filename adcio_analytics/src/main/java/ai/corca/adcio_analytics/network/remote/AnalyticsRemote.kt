bcfba2185d1d2d8b11c36ab44a3c1c6a4b12e19f
yoohyunmyeong <aahspringaa4@naver.com>
2024-07-05 17:36:11 +0900

    feat: version up

--- adcio_analytics/src/main/java/ai/corca/adcio_analytics/network/remote/AnalyticsRemote.kt
+++ adcio_analytics/src/main/java/ai/corca/adcio_analytics/network/remote/AnalyticsRemote.kt
@@ -17,7 +17,7 @@ import retrofit2.Response
 internal class AnalyticsRemote {
 
     private val networkSuccessRange = 200 until 300
-    private val sdkVersion = "1.4.0"
+    private val sdkVersion = "1.4.2"
 
     fun onImpression(
         sessionId: String,
